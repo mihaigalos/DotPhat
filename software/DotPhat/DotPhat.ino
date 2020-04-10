e00a8b298a6dc0fb4714b6153f1edda29fbeca75
Mihai Galos <mihaigalos@gmail.com>
2020-04-10 21:03:03 +0200

    Refactor eeprom config to separate file

--- software/DotPhat/DotPhat.ino
+++ software/DotPhat/DotPhat.ino
@@ -14,6 +14,8 @@
 #include "supercapacitor.h"
 #include "unix_timestamp.h"
 
+#include "eeprom_config.h"
+
 enum class ApplicationsStatus {
   Unknown,
   Idle,
@@ -40,69 +42,6 @@ static constexpr uint8_t kTRXLed = 9;
 static constexpr uint8_t kInterruptPin = 3;
 static constexpr uint8_t kClockPrescaler = CLOCK_PRESCALER_16;
 
-static constexpr EEPROMMetadata current_configuration{
-    MetadataVersion{
-        .major = 1,
-        .minor = 0,
-        .patch = 0,
-    },
-    DeviceType::DotShine,
-    SoftwareVersion{
-     .major = 1,
-     .minor = 0,
-     .patch = 0},
-    TimeZoneInfo{
-     .timezone_sign = 1,
-     .utc_offset = 1,
-     .is_daylight_saving_active = LOCATION_DAYLIGHT_SAVING,
-     .is_china_time = 0},
-    {// software_version_last_updated_timestamp
-     static_cast<uint8_t>(UNIX_TIMESTAMP >> 24),
-     static_cast<uint8_t>(UNIX_TIMESTAMP >> 16),
-     static_cast<uint8_t>(UNIX_TIMESTAMP >> 8),
-     static_cast<uint8_t>(UNIX_TIMESTAMP)},
-    HardwareVersion{
-     .major = 1,
-     .minor = 1,
-     .patch = 0},
-    TimeZoneInfo{
-     .timezone_sign = 1,
-     .utc_offset = 8,
-     .is_daylight_saving_active = 1,
-     .is_china_time = 1},
-    {// hardware_version_timestamp : add 8 hours to the PCB manufactureing time
-     // in China
-     0x5A, 0xBF, 0x78, 0x1D},
-    {"Board1"},
-    UEnergyInfo{
-     {InstalledCapacity::None, 0}},
-    GPSPosition{
-        48.189756, 11.572531
-    },
-    {"No xtra loc info"},
-    InstalledDevices{
-     .temperature_sensor = 1,
-     .ultraviolet_sensor = 1,
-     .eeprom = 1,
-     .piezo_speaker = 0,
-     .crypto_module = 0,
-     .high_precision_time_reference = 1,
-     .reset_pushbutton = 1,
-     .act_pushbutton = 1},
-    InstalledDevices2{
-     .usb = 1,
-     .external_antenna = 0,
-     .antenna_calibration = 0,
-     .solar_panel = 1},
-    InstalledLeds{
-     .usb_power = 1,
-     .outA = 1,
-     .outB = 1,
-     .reset = 1,
-     .rgb = 1,
-     .tx = 1,
-     .rx = 1,
-     .reserved = 1}};
     
 
 EEPROMMetadata e2prom_metadata;
---
+++ software/DotPhat/eeprom_config.h
@@ -0,0 +1,66 @@
+#pragma once
+
+
+static constexpr EEPROMMetadata current_configuration{
+    MetadataVersion{
+        .major = 1,
+        .minor = 0,
+        .patch = 0,
+    },
+    DeviceType::DotPhat,
+    SoftwareVersion{
+     .major = 1,
+     .minor = 0,
+     .patch = 0},
+    TimeZoneInfo{
+     .timezone_sign = 1,
+     .utc_offset = 1,
+     .is_daylight_saving_active = LOCATION_DAYLIGHT_SAVING,
+     .is_china_time = 0},
+    {// software_version_last_updated_timestamp
+     static_cast<uint8_t>(UNIX_TIMESTAMP >> 24),
+     static_cast<uint8_t>(UNIX_TIMESTAMP >> 16),
+     static_cast<uint8_t>(UNIX_TIMESTAMP >> 8),
+     static_cast<uint8_t>(UNIX_TIMESTAMP)},
+    HardwareVersion{
+     .major = 1,
+     .minor = 1,
+     .patch = 0},
+    TimeZoneInfo{
+     .timezone_sign = 1,
+     .utc_offset = 8,
+     .is_daylight_saving_active = 1,
+     .is_china_time = 1},
+    {// hardware_version_timestamp : add 8 hours to the PCB manufactureing time
+     // in China
+     0x5A, 0xBF, 0x78, 0x1D},
+    {"Board1"},
+    UEnergyInfo{
+     {InstalledCapacity::None, 0}},
+    GPSPosition{
+        48.189756, 11.572531
+    },
+    {"No xtra loc info"},
+    InstalledDevices{
+     .temperature_sensor = 1,
+     .ultraviolet_sensor = 1,
+     .eeprom = 1,
+     .piezo_speaker = 0,
+     .crypto_module = 0,
+     .high_precision_time_reference = 1,
+     .reset_pushbutton = 1,
+     .act_pushbutton = 1},
+    InstalledDevices2{
+     .usb = 1,
+     .external_antenna = 0,
+     .antenna_calibration = 0,
+     .solar_panel = 1},
+    InstalledLeds{
+     .usb_power = 1,
+     .outA = 1,
+     .outB = 1,
+     .reset = 1,
+     .rgb = 1,
+     .tx = 1,
+     .rx = 1,
+     .reserved = 1}};
