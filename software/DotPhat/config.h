6b63b3e1414a8b9f312c68a44c4cdec5b9e5dd04
Mihai Galos <mihaigalos@gmail.com>
2020-04-10 21:19:49 +0200

    Move constants to config.h

---
+++ software/DotPhat/config.h
@@ -0,0 +1,15 @@
+#pragma once 
+
+static constexpr auto kOwnId = 0x10;
+static constexpr auto kMaxRfPower = 31;
+
+static constexpr uint8_t kRedLed = 8;
+static constexpr uint8_t kBlueLed = 0;
+static constexpr uint8_t kGreenLed = 1;
+
+static constexpr uint8_t kOutABLed = 10;
+
+static constexpr uint8_t kTRXLed = 9;
+
+static constexpr uint8_t kInterruptPin = 3;
+static constexpr uint8_t kClockPrescaler = CLOCK_PRESCALER_16;
